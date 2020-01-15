02bbf1359193ef6bfc117e5a8f8d242bdb3ab489
Austin Osorio <osorio_austin@yahoo.com>
2020-01-15 05:15:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,11 +34,9 @@ def consolidate_cart(cart)
 
           new_cart[new_cart_i][:count] += 1
 
-        else
-
-          new_cart_i += 1
-
         end
+
+        new_cart_i += 1
       end
     end
 
