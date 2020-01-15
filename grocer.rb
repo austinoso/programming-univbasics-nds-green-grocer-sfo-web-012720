0f03f4711592cb622179d42a3f988d5169a95f6f
Austin Osorio <osorio_austin@yahoo.com>
2020-01-15 07:07:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -95,11 +95,11 @@ def apply_clearance(cart)
 
   # clearance_cart = []
   # i = 0
-  # 
+  #
   # while i < cart.length do
-  # 
-  #   if !cart[i] 
-  #   
+  #
+  #   if !cart[i]
+  #
   # end
 
 end
