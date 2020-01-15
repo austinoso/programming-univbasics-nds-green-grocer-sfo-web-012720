08ba3b2eb636108551e80450c56c57e3ac8c95a7
Austin Osorio <osorio_austin@yahoo.com>
2020-01-15 05:31:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -70,9 +70,9 @@ def apply_coupons(cart, coupons)
        apply_coupons << {
 
          :item => cart[cart_index][:item],
-         :price => coupons[coupons_index][:cost] / coupon_num,
+         :price => coupons[coupons_index][:cost] / coupons[coupons_index][:num],
          :clearance => cart[cart_index][:clearance],
-         :count => coupon_num
+         :count => coupons[coupons_index][:num]
 
        }
 
